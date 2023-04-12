d93689a6a15039ca6b8dc1b048e67bfb0d7b366b
Guilherme Lima <guirezendel@icloud.com>
2023-04-12 11:32:50 -0300

    Criado a estrutura da loja

--- Shop.java
+++ Shop.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 
 public class Shop {
     public static void main(String[] args) {
         
+        Scanner lerTeclado = new Scanner(System.in);
+        System.out.println("Seja bem-vindo ao Shop Atitus!");
+        System.out.println("Para continuar digite a opção desejada:");
+        System.out.println("1 - Adicionar produto ao carrinho");
+        System.out.println("2 - Finalizar compra");
+        System.out.println("3 - Sair" );
+
         
+
     }    
 }
