717bb534a11604d1fa9db2670c46101ac41c4565
Guilherme Lima <guirezendel@icloud.com>
2023-04-14 15:19:13 -0300

    Bugs corrigidos e opção de listar produtos agora com nome

--- Shop.java
+++ Shop.java
@@ -5,11 +5,14 @@ public class Shop {
     public static void main(String[] args) {
 
         Scanner lerTeclado = new Scanner(System.in);
-        System.out.println("Seja bem-vindo ao Shop Atitus!");
         ArrayList<Double> listaProdutos = new ArrayList<>();
+        ArrayList<Integer> guardaNumero = new ArrayList<>();
         listaProdutos.add(2500.00);
         listaProdutos.add(3282.34);
         listaProdutos.add(8235.28);
+
+
+        System.out.println("Seja bem-vindo ao Shop Atitus!");
 
         ArrayList<Double> carrinho = new ArrayList<>();
 
@@ -23,8 +26,10 @@ public class Shop {
             System.out.println("4 - Finalizar compra");
             System.out.println("5 - Sair");
             System.out.print("Opção: ");
+
             int opcao = lerTeclado.nextInt();
             double somaFinal = 0;
+
             if (opcao == 5) {
                 break;
             } else {
@@ -35,6 +40,8 @@ public class Shop {
                         System.out.println("1 - ZOTAC RTX 2060 6GB - R$ 2.500,00 ");
                         System.out.println("2 - ZOTAC RTX 3060 Ti 8GB - R$ 3.282,34");
                         System.out.println("3 - ZOTAC RTX 4070 Ti 12GB - R$ 8.235,28");
+                        System.out.print("Pressione OK para continuar...");
+                        lerTeclado.next();
                         break;
 
                     case 2:
@@ -42,21 +49,43 @@ public class Shop {
                         System.out.println("Favor indicar o número do produto (1,2 ou 3)");
                         System.out.print("Número do produto: ");
                         int produto = lerTeclado.nextInt();
+                        guardaNumero.add(produto);
                         carrinho.add(listaProdutos.get(produto - 1));
                         break;
-                    
+
                     case 3:
-                        System.out.println(carrinho);
+                        if(carrinho.size() == 0){
+
+                            System.out.println("Sua lista está vazia!");
+                        }
+                        for (int n = 0; n < guardaNumero.size(); n++) {
+
+                            if (guardaNumero.get(n) == 1) {
+
+                                System.out.println("ZOTAC RTX 2060 6GB");
+                            } else if (guardaNumero.get(n) == 2) {
+                                System.out.println("ZOTAC RTX 3060 Ti 8GB");
+                            } else if (guardaNumero.get(n) == 3) {
+                                System.out.println("ZOTAC RTX 4070 Ti 12GB");
+                            }
+                        }
                         break;
 
                     case 4:
-                        for(int n = 0; n < carrinho.size(); n++) {
+                        for (int n = 0; n < carrinho.size(); n++) {
                             somaFinal = somaFinal + carrinho.get(n);
                         }
-                        System.out.printf("O total de sua compra é de R$ %.2f \n", somaFinal);
-                        System.out.println("Agradeçemos sua compra, volte sempre!");
-                        finalizador = 1;
-                        break;
+                        if(carrinho.size() > 0){
+                            System.out.printf("O total de sua compra é de R$ %.2f \n", somaFinal);
+                            System.out.println("Agradeçemos sua compra, volte sempre!");
+                            finalizador = 1;
+                            break;
+
+                        } else {
+                            System.out.println("Você não possuí nenhum item em seu carrinho, caso deseje sair da loja digite 5!");
+                            break;
+                            
+                        }
 
                     default:
                         System.out.println("Número inválido!");
@@ -65,5 +94,4 @@ public class Shop {
             }
         }
     }
-
 }
